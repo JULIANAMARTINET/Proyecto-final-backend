69c84ef1be1d61def2590512ca573389ef6138c0
juliana martinet <martinetjuliana@gmail.com>
2023-03-07 10:38:34 -0300

    actualizacion

--- src/routers/Product/index.js
+++ src/routers/Product/index.js
@@ -11,7 +11,7 @@ router.get("/",isValidAuthToken, ProductController.getAll);
 
 router.get("/:id", ProductController.getById);
 
-router.post("/",  ProductController.createProduct);
+router.post("/", verifyRole, ProductController.createProduct);
 
 router.delete("/:id", ProductController.deleteById);
 
