2332def754d6a85060c4944b415d7aa0cad9c4af
juliana martinet <martinetjuliana@gmail.com>
2023-03-20 00:48:36 -0300

    subiendo railway

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,5 +4,5 @@ export { ERRORS_UTILS } from "./error-utils.js";
 export { LOGGER_UTILS } from "./logger-utils.js";
 export { JWT_UTILS } from "./jwt-utils.js";
 export { EMAIL_UTILS } from "./email-utils.js";
-export { BCRYPT_VALIDATION } from './bcrypt.js'
-export { TEMPLATE } from './purchase.js'
+export { BCRYPT_VALIDATION } from './bcrypt.js';
+export { TEMPLATE } from './purchase.js';
