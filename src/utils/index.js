580a5d6b030bf68cfbca8aeef8e82ef6baf6872f
juliana martinet <martinetjuliana@gmail.com>
2023-03-20 01:05:12 -0300

    purchase.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ export { JOI_VALIDATOR } from "./joi-validator.js";
 export { ERRORS_UTILS } from "./error-utils.js";
 export { LOGGER_UTILS } from "./logger-utils.js";
 export { JWT_UTILS } from "./jwt-utils.js";
-export { TEMPLATE } from './purchase.js'
+export { TEMPLATE } from './purchase.js';
 export { EMAIL_UTILS } from "./email-utils.js";
 export { BCRYPT_VALIDATION } from './bcrypt.js';
 
