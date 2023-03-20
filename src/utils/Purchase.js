f61723fc944d8f286bebd88cee8363e12d121dac
juliana martinet <martinetjuliana@gmail.com>
2023-03-20 00:58:34 -0300

    err purchase.js

--- src/utils/Purchase.js
+++ src/utils/Purchase.js
@@ -1,9 +1,11 @@
-const getPurchaseTemplate = (email, listado, total) => `
+const getPurchaseTemplate = (email, listado, total) => {
+  `
      <h3>Pago realizado por el usuario ${email} </h3>
      <p> Detalle de compra:</p>
      <ul>
          ${listado}
      </ul>
-     <p>Total $${total}<p>`
+     <p>Total $${total}<p>`;
+};
 
-     export const TEMPLATE = { getPurchaseTemplate }
+export const TEMPLATE = { getPurchaseTemplate };
